9e8663650dfce9ef1b3b8cd97bdb918b12c44f1e
rinnifox <karina.m.pats@gmail.com>
2018-08-07 17:56:57 +0300

    added HW3

---
+++ 3_HW_Iterators_Generators/HW3.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+from collections import Counter
+
+
+def hardlink_check(directory_path: str) -> bool:
+
+    directory = Path(directory_path)
+
+    if not directory.exists():
+        return False
+
+    inodes = list()
+    hardlinks = list()
+
+    for file in directory.iterdir():
+        inodes.append(os.stat(file).st_ino)
+
+    counter = Counter(inodes)
+
+    for key, value in counter.items():
+        if value > 1:
+            hardlinks.append(key)
+
+    if hardlinks:
+        return True
+    else:
+        return False
