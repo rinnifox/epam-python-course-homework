5186b82b35674b5eccf88d32dc87ebc0eeaeba17
rinnifox <karina.m.pats@gmail.com>
2018-08-10 12:44:30 +0300

    corrected mistakes

--- 2_HW_Functions_1/HW2.py
+++ 2_HW_Functions_1/HW2.py
@@ -2,9 +2,7 @@ def make_it_count(func, counter_name):
 
     def wrapper(*args, **kwargs):
         nonlocal counter_name
-
         globals()[counter_name] += 1
-
         return func(*args, **kwargs)
 
     return wrapper
--- 2_HW_Functions_1/HW3.py
+++ 2_HW_Functions_1/HW3.py
@@ -2,22 +2,17 @@ def atom(var=None):
 
     def get_value():
         nonlocal var
-        print('The value of variable = ', var)
         return var
 
     def set_value(new_value):
         nonlocal var
         var = new_value
-        print('New value of variable = ', var)
         return var
 
     def process_value(*args):
         nonlocal var
-        if var:
-            for func in args:
-                var = func(var)
-            print('New value of variable = ', var)
-
+        for func in args:
+            var = func(var)
         return var
 
     return get_value, set_value, process_value
