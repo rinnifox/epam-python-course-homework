6735f1317cd5d0ae9178b6a5181cda0738d94394
rinnifox <karina.m.pats@gmail.com>
2018-08-13 20:11:01 +0300

    deleted nonlocal

--- 2_HW_Functions_1/HW2.py
+++ 2_HW_Functions_1/HW2.py
@@ -1,7 +1,6 @@
 def make_it_count(func, counter_name):
 
     def wrapper(*args, **kwargs):
-        nonlocal counter_name
         globals()[counter_name] += 1
         return func(*args, **kwargs)
 
