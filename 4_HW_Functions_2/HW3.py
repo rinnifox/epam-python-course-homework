3c1804d2eb5646d153630ab6212babcebd5ab01b
rinnifox <karina.m.pats@gmail.com>
2018-08-13 11:55:26 +0300

    corrected mistakes

--- 4_HW_Functions_2/HW3.py
+++ 4_HW_Functions_2/HW3.py
@@ -2,16 +2,21 @@ def count_points(*args):
     (x1, y1), (x2, y2),(x3, y3) = args
     x_list = [x1, x2, x3]
     y_list = [y1, y2, y3]
+    print(x_list)
+    print(y_list)
 
     points_counter = 0
 
     for i in range(min(x_list), max(x_list)+1):
-        for j in range(min(x_list), max(y_list)+1):
+        for j in range(min(y_list), max(y_list)+1):
             w1 = (i - x1) * (y2 - y1) - (j - y1) * (x2 - x1)
             w2 = (i - x2) * (y3 - y2) - (j - y2) * (x3 - x2)
             w3 = (i - x3) * (y1 - y3) - (j - y3) * (x1 - x3)
+            print('coord', i, j)
+            print(w1,w2,w3)
 
             if (w1 < 0 and w2 < 0 and w3 < 0) or (w1 > 0 and w2 > 0 and w3 > 0) or w1 == 0 or w2 == 0 or w3 == 0:
                 points_counter += 1
+                print ('a')
 
     return points_counter
