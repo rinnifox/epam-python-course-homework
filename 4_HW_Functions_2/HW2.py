0f588203d7265270f0e0be55510f83b555660c6f
rinnifox <karina.m.pats@gmail.com>
2018-08-14 14:44:14 +0300

    corrected accuracy

--- 4_HW_Functions_2/HW2.py
+++ 4_HW_Functions_2/HW2.py
@@ -1,12 +1,24 @@
 def newton_sqrt(x, accuracy=0.0, approx=1):
     newapprox = (approx + x / approx) / 2
-    if abs(newapprox - approx) > accuracy:
+
+    if abs(newapprox - approx) > 0:
         return newton_sqrt(x, approx=newapprox, accuracy=accuracy)
+
     else:
-        return newapprox
+        if accuracy == 0.0:
+            return newapprox
+        else:
+            s = str(accuracy)
+            r = abs(s.find('.') - len(s)) - 1
+            return round(newapprox, ndigits=r)
 
 
 print(newton_sqrt(5))
 print(newton_sqrt(5, accuracy=0.001))
 print(newton_sqrt(5, accuracy=0.01))
 print(newton_sqrt(5, accuracy=0.1))
+
+
+print(newton_sqrt(4/1000000))
+print(newton_sqrt(4/1000000, accuracy=0.01))
+print(newton_sqrt(4/1000000, accuracy=0.1))
