fccba93ffc3e6209daec629aa4294b26d3a01573
rinnifox <karina.m.pats@gmail.com>
2018-08-15 16:50:33 +0300

    corrected accuracy

--- 4_HW_Functions_2/HW2.py
+++ 4_HW_Functions_2/HW2.py
@@ -1,5 +1,6 @@
 def newton_sqrt(x, accuracy=0.0, percentage=100, approx=1):
     newapprox = (approx + x / approx) / 2
+
     if percentage == 100:
         if abs(newapprox - approx) > 0:
             return newton_sqrt(x, approx=newapprox, accuracy=accuracy, percentage=percentage)
@@ -11,8 +12,13 @@ def newton_sqrt(x, accuracy=0.0, percentage=100, approx=1):
     if accuracy == 0.0:
         return newapprox
     else:
-        s = str(accuracy)
-        r = abs(s.find('.') - len(s)) - 1
+        s = '{}'.format(accuracy)
+
+        if 'e' in s:
+            r = int(s[-2:])
+        else:
+            r = abs(s.find('.') - len(s)) - 1
+
         return round(newapprox, ndigits=r)
 
 
@@ -30,3 +36,12 @@ print(newton_sqrt(4/1000000, accuracy=0.1))
 print(newton_sqrt(10000, percentage=100))
 print(newton_sqrt(10000, percentage=75))
 print(newton_sqrt(10000, percentage=30))
+
+
+print(newton_sqrt(5/10000000000, 0.0000001))
+print(newton_sqrt(5/10000000000, 0.01))
+print(newton_sqrt(5/10000000000, 0.001))
+
+print(newton_sqrt(5/100000000, 0.00000001))
+print(newton_sqrt(5/100000000, 0.01))
+print(newton_sqrt(5/100000000, 0.001))
