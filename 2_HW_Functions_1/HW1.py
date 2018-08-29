3a47c23cc5ae8a4cf643e72fae388ef641a0fd2c
rinnifox <karina.m.pats@gmail.com>
2018-08-29 16:07:18 +0300

    corrected inspect

--- 2_HW_Functions_1/HW1.py
+++ 2_HW_Functions_1/HW1.py
@@ -9,12 +9,26 @@ def partial(func, *fixated_args, **fixated_kwargs):
         fixated_kwargs.update(kwargs)
         return func(*fixated_args, **fixated_kwargs)
 
-    arg_names = inspect.getcallargs(func, fixated_args, fixated_kwargs)
+    setargs = {}
+    otherargs = []
+
+    setargs_names = inspect.getfullargspec(func)[0]
+
+    if len(setargs_names) > len(fixated_args):
+        someargs = fixated_args
+    else:
+        someargs = setargs_names
+
+    for i in range(len(someargs)):
+        setargs.update({setargs_names[i]: fixated_args[i]})
+
+    if len(fixated_args) > len(setargs_names):
+        otherargs = fixated_args[len(setargs):]
 
     wrapper.__name__ = 'partial_{}'.format(func.__name__)
     wrapper.__doc__ = """ 
-    A partial implementation of {} with pre-applied arguements being: {}
-    """.format(func.__name__, arg_names)
+    A partial implementation of {} with pre-applied arguements being: position_args = {}, *args = {}, **kwargs = {}
+    """.format(func.__name__, setargs, otherargs, fixated_kwargs)
 
     return wrapper
 
@@ -25,15 +39,17 @@ def avg(a, b):
     return (a+b)/2
 
 
-def add(x, y):
-    return x + y
+def random_func(a, *args, **kwargs):
+    zero = a + 3
+    first = sum(args)/2
+    second = kwargs
+    return zero, first, second
 
 
 _avg = partial(avg, 4, 3)
 print(_avg.__name__)
 print(_avg.__doc__)
 print(_avg())
-
 
 _avg = partial(avg, 4)
 print(_avg.__name__)
@@ -45,8 +61,8 @@ print(_avg.__name__)
 print(_avg.__doc__)
 print(_avg(3, 4))
 
+_random_func = partial(random_func, 4, 3, 6, x=5)
+print(_random_func.__name__)
+print(_random_func.__doc__)
+print(_random_func())
 
-p_add = partial(add, 2)
-print(p_add.__name__)
-print(p_add.__doc__)
-print(p_add(4))
