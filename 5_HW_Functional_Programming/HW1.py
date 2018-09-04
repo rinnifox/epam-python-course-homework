e6b1e7c9ab0d01889f02a124952ad3ecc94d3f3b
rinnifox <karina.m.pats@gmail.com>
2018-09-04 10:59:50 +0300

    shortened time

--- 5_HW_Functional_Programming/HW1.py
+++ 5_HW_Functional_Programming/HW1.py
@@ -8,7 +8,7 @@ res1 = reduce(lambda a, b: a+b, (map(lambda x: x**2, range(10**3))))
 
 res2 = reduce(lambda a, b: a+b, filter(lambda x: int(sqrt(x)) == float(sqrt(x)), range(10**6)))
 
-res3 = reduce(lambda a, b: a+b, ([x**2 for x in range(10**3)]))
+res3 = reduce(lambda a, b: a+b, [x**2 for x in range(10**3)])
 
 print(res1)
 print(res2)
