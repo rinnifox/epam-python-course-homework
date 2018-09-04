48638373194f4c95246516e0041a6106709b7ca2
rinnifox <karina.m.pats@gmail.com>
2018-09-04 10:52:27 +0300

    shortened time

--- 5_HW_Functional_Programming/HW1.py
+++ 5_HW_Functional_Programming/HW1.py
@@ -1,9 +1,19 @@
 from math import sqrt
 from functools import reduce
+import time
 
+s = time.time()
 
-res1 = reduce(lambda a, b: a+b, map(lambda x: x if int(sqrt(x)) == float(sqrt(x)) else 0, range(10**6)))
+res1 = reduce(lambda a, b: a+b, (map(lambda x: x**2, range(10**3))))
 
 res2 = reduce(lambda a, b: a+b, filter(lambda x: int(sqrt(x)) == float(sqrt(x)), range(10**6)))
 
-res3 = reduce(lambda a, b: a+b, [x for x in range(10**6) if int(sqrt(x)) == float(sqrt(x))])
+res3 = reduce(lambda a, b: a+b, ([x**2 for x in range(10**3)]))
+
+print(res1)
+print(res2)
+print(res3)
+
+end = time.time()
+
+print('time =', end-s)
